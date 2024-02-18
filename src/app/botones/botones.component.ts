b8da75913aaf1bc344c02541c51dcd02cc991c50
Raul Pimentel Sanchez <r.pimente@praxisglobe.net>
2024-02-18 14:13:02 -0600

    add botones

---
+++ src/app/botones/botones.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-botones',
+  standalone: true,
+  imports: [],
+  templateUrl: './botones.component.html',
+  styleUrl: './botones.component.css'
+})
+export class BotonesComponent {
+
+}
