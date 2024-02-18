c6f19270daa0ef2cf2672a60fd85eca4cdc265ef
Raul Pimentel Sanchez <r.pimente@praxisglobe.net>
2024-02-18 01:30:16 -0600

    Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,47 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import Swal from 'sweetalert2';
+import {ContadorComponent} from "./contador/contador.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContadorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-proyect';
+
+  Update(mensaje: any) {
+    Swal.fire({
+      title: "Deseas guardar cambios?",
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: "Guardar",
+      denyButtonText: `No guardar`,
+      cancelButtonText: "Cancelar"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: 'Guardado',
+          text: mensaje,
+          showConfirmButton: false,
+          timer: 1000
+        })
+      } else if (result.isDenied) {
+        Swal.fire({
+          title: 'No guardado',
+          text: mensaje,
+          showConfirmButton: false,
+          timer: 1000
+        })
+      }
+    });
+  }
+
+  UpdateUser (mensaje: any) {
+    alert(mensaje);
+  }
+
 }
